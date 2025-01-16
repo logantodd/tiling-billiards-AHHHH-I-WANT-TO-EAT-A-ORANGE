4071459eede1c112133427f87edb3b743e97fee3
Lael Costa <lael.shelton.costa@gmail.com>
2025-01-16 18:08:41 -0500

    Rebased.

--- src/main/java/com/laelcosta/tilingbilliards/TilingBilliardsApplication.java
+++ src/main/java/com/laelcosta/tilingbilliards/TilingBilliardsApplication.java
@@ -5,13 +5,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class TilingBilliardsApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(TilingBilliardsApplication.class.getResource("tiling-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1400, 1000);
+        GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        int width = gd.getDisplayMode().getWidth();
+        int height = gd.getDisplayMode().getHeight();
+        Scene scene = new Scene(fxmlLoader.load(), width, height);
         stage.setTitle("Tiling Billiards");
         stage.setScene(scene);
         stage.show();
